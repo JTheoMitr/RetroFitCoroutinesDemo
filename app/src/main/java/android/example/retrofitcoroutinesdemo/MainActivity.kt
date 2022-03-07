9ee38c330518a37b35bda7dab21255f2b61c743b
John <jmitropoulos@alumni.berklee.edu>
2022-03-07 11:58:49 -0600

    Initial commit

---
+++ app/src/main/java/android/example/retrofitcoroutinesdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package android.example.retrofitcoroutinesdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
