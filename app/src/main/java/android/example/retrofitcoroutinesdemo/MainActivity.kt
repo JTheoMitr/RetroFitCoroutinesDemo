5bbeda51b18e196545fa77cf7bb0edeae959da07
John <jmitropoulos@alumni.berklee.edu>
2022-03-07 22:41:19 -0600

    now fetching and displaying cat facts

--- app/src/main/java/android/example/retrofitcoroutinesdemo/MainActivity.kt
+++ app/src/main/java/android/example/retrofitcoroutinesdemo/MainActivity.kt
@@ -1,11 +1,72 @@
 package android.example.retrofitcoroutinesdemo
 
+import android.example.retrofitcoroutinesdemo.api.CatJson
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.awaitResponse
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+const val BASE_URL = "https://cat-fact.herokuapp.com"
 
 class MainActivity : AppCompatActivity() {
+
+    private var TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getCurrentData()
+
+        layout_generate_new_fact.setOnClickListener {
+            getCurrentData()
+        }
+
+
+    }
+
+    private fun getCurrentData() {
+
+        tv_textView.visibility = View.INVISIBLE
+        tv_timeStamp.visibility = View.INVISIBLE
+        progressBar.visibility = View.VISIBLE
+
+        val api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiRequests::class.java)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val response = api.getCatFacts().awaitResponse()
+            if (response.isSuccessful) {
+                val data = response.body()!!
+                Log.d(TAG, data.text)
+
+                withContext(Dispatchers.Main) {
+                    tv_textView.visibility = View.VISIBLE
+                    tv_timeStamp.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
+
+                    tv_textView.text = data.text
+                    tv_timeStamp.text = data.createdAt
+                }
+            }
+        }
+
+
+
+
+
     }
 }
